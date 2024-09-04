9e684e3825357224dd70ca2a9f71264c1937c759
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-09-04 11:45:05 +0430

    Added: enhanced like when user changed password after token issued or if this token related user is exist.

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -4,14 +4,30 @@ const asyncHandler = require('./asyncHandler.js');
 
 const authenticate = asyncHandler(async (req, res, next) => {
   let token;
-
-  // Read JWT from the 'jwt' cookie
+  // 1) Read JWT from the 'jwt' cookie
   token = req.cookies.jwt;
 
   if (token) {
     try {
+      // 2) Verification token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userId).select('-password');
+      // 3) Check if user is still there
+      const currentUser = await User.findById(decoded.userId).select(
+        '-password'
+      );
+      if (!currentUser) {
+        res.status(401);
+        throw new Error('The user belonging to this token no longer exist!');
+      }
+
+      // 4) Check if the user changed password after token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        res.status(401);
+        throw new Error(
+          'User recently changed the password, please try again!'
+        );
+      }
+      req.user = currentUser;
       next();
     } catch (error) {
       res.status(401);
