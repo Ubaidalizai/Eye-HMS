b9e66e55614b700abdc993829a48c0f23519d015
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-09-08 14:11:42 +0430

    Added: totalNetIncome functionality

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -2,20 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
-const authenticate = require('../middlewares/authMiddleware');
+const { authenticate } = require('../middlewares/authMiddleware');
 
 router.route('/').get(userController.getAllUsers);
 router.route('/register').post(userController.registerUser);
 router.route('/login').post(userController.loginUser);
 router.route('/logout').post(userController.logoutCurrentUser);
-router
-  .route('/updatePassword')
-  .patch(authenticate, userController.updatePassword);
+router.patch('/updatePassword', authenticate, userController.updatePassword);
+router.post('/forgotPassword', authenticate, userController.forgotPassword);
+router.patch(
+  '/resetPassword/:token',
+  authenticate,
+  userController.resetPassword
+);
 
 router.patch(
   '/updateMe',
+  authenticate,
   userController.uploadUserPhoto,
+  authenticate,
   userController.resizeUserPhoto,
+  authenticate,
   userController.updateUserPhoto
 );
 
