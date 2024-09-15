6621fed39c3a56b4e942f91091e8c06d38fcdf5d
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-09-15 15:41:23 +0430

    Added: sales routes

---
+++ Backend/routes/salesRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const router = express.Router();
+const {
+  sellDrugs,
+  getAllSales,
+  getOneMonthSales,
+  getMonthlySales,
+} = require('../controllers/salesController');
+const { authenticate } = require('../middlewares/authMiddleware');
+
+router.use(authenticate); // Enable authentication middleware for all routes in this file
+
+// Get Sales Monthly Data
+router.get('/one-month-sales/:year/:month', getOneMonthSales);
+router.get('/monthly-sales/', getMonthlySales);
+
+// Add Sales
+router.route('/').get(getAllSales).post(sellDrugs);
+
+module.exports = router;
