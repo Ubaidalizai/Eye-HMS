b3c3e9d16b3e440a9e92172b64053d9ab324d0e6
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-09-15 15:29:03 +0430

    Added: sales model

---
+++ Backend/models/salesModel.js
@@ -0,0 +1,40 @@
+// saleModel.js
+const mongoose = require('mongoose');
+const Pharmacy = require('./pharmacyModel');
+const User = require('./userModel');
+
+const saleSchema = new mongoose.Schema({
+  soldItems: [
+    {
+      drugId: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Pharmacy',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: [true, 'A sale must have a total income'],
+      },
+      income: Number,
+    },
+  ],
+  totalIncome: {
+    type: Number,
+    required: [true, 'A sale must have a total income'],
+  },
+  totalNetIncome: {
+    type: Number,
+    required: [true, 'A sale must have a total net income'],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  soldBy: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+});
+
+const Sale = mongoose.model('Sale', saleSchema);
+module.exports = Sale;
