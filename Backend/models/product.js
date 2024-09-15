88b00fe5df13fd217e67d423623c7edc218d63d6
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-09-15 17:00:08 +0430

    Changed: type to category

--- Backend/models/product.js
+++ Backend/models/product.js
@@ -20,7 +20,7 @@ const ProductSchema = new mongoose.Schema(
       required: true,
     },
     description: String,
-    type: {
+    category: {
       type: String,
       enum: ['drug', 'sunglasses'],
       required: true,
