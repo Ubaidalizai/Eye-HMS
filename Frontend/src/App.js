ff11987e82826aa5eeb0ecf8cd0a5e6a461d7b22
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-09-28 15:19:33 +0430

    Remove: unicessary route

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,27 +1,27 @@
-import React from "react";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Inventory from "./pages/Inventory";
-import NoPageFound from "./pages/NoPageFound";
-import AuthContext from "./AuthContext";
-import ProtectedWrapper from "./ProtectedWrapper";
-import { useEffect, useState } from "react";
-import Store from "./pages/Store";
-import Sales from "./pages/Sales";
-import PurchaseDetails from "./pages/PurchaseDetails";
-import Move from "./pages/Move";
-import Pharmacy from "./pages/Pharmacy";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import React from 'react';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Inventory from './pages/Inventory';
+import NoPageFound from './pages/NoPageFound';
+import AuthContext from './AuthContext';
+import ProtectedWrapper from './ProtectedWrapper';
+import { useEffect, useState } from 'react';
+import Store from './pages/Store';
+import Sales from './pages/Sales';
+import PurchaseDetails from './pages/PurchaseDetails';
+import Move from './pages/Move';
+import Pharmacy from './pages/Pharmacy';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const App = () => {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState('');
   const [loader, setLoader] = useState(true);
-  let myLoginUser = JSON.parse(localStorage.getItem("user"));
+  let myLoginUser = JSON.parse(localStorage.getItem('user'));
   // console.log("USER: ",user)
 
   useEffect(() => {
@@ -30,7 +30,7 @@ const App = () => {
       setLoader(false);
       // console.log("inside effect", myLoginUser)
     } else {
-      setUser("");
+      setUser('');
       setLoader(false);
     }
   }, [myLoginUser]);
@@ -42,7 +42,7 @@ const App = () => {
 
   const signout = () => {
     setUser(null);
-    localStorage.removeItem("user");
+    localStorage.removeItem('user');
   };
 
   let value = { user, signin, signout };
@@ -52,9 +52,9 @@ const App = () => {
       <div
         style={{
           flex: 1,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
         <h1>LOADING...</h1>
@@ -82,7 +82,6 @@ const App = () => {
               <Route path="/sales" element={<Sales />} />
               <Route path="/manage-store" element={<Store />} />
               <Route path="/move" element={<Move />} />
-              <Route path="/*" element={<Move />} />
               <Route path="/pharmacy" element={<Pharmacy />} />
             </Route>
             <Route path="*" element={<NoPageFound />} />
