296d563dc5051777633072a1d8e8fca5fd8a21d4
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-09-29 09:21:27 +0430

    Fix: bug while adding product stock

--- Backend/controllers/purchaseStock.js
+++ Backend/controllers/purchaseStock.js
@@ -2,19 +2,16 @@ const Purchase = require('../models/purchase');
 const Product = require('../models/product');
 const asyncHandler = require('../middlewares/asyncHandler');
 
-const purchaseStock = asyncHandler( async (productID, purchaseStockData) => {
+const purchaseStock = asyncHandler(async (productID, purchaseStockData) => {
   // Updating Purchase stock
   try {
-    const myProductData = await Product.findOne({ _id: productID });
-    let myUpdatedStock = parseInt(myProductData.stock) + purchaseStockData;
-
-    const PurchaseStock = await Product.findByIdAndUpdate(
-      { _id: productID },
-      {
-        stock: myUpdatedStock,
-      },
-      { new: true }
-    );
+    const product = await Product.findById({ _id: productID });
+    if (!product) {
+      res.status(404);
+      throw new Error('The product not found, for updating stoke');
+    }
+    product.stock += Number(purchaseStockData);
+    await product.save();
   } catch (error) {
     console.error('Error updating Purchase stock ', error);
   }
