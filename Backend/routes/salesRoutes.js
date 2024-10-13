796d1d5f644528e80036acb253d1767412a08b9e
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-10-13 14:17:04 +0430

    Change: route name getMonthlySales

--- Backend/routes/salesRoutes.js
+++ Backend/routes/salesRoutes.js
@@ -5,16 +5,16 @@ const router = express.Router();
 const {
   sellItems,
   getAllSales,
+  getOneYearSales,
   getOneMonthSales,
-  getMonthlySales,
 } = require('../controllers/salesController');
 const { authenticate } = require('../middlewares/authMiddleware');
 
 router.use(authenticate); // Enable authentication middleware for all routes in this file
 
 // Get Sales Monthly Data
-router.get('/one-month-sales/:year/:month', getOneMonthSales);
-router.get('/monthly-sales/', getMonthlySales);
+router.get('/:year/:month', getOneMonthSales);
+router.get('/:year', getOneYearSales);
 
 // Add Sales
 router.route('/').get(getAllSales).post(sellItems);
