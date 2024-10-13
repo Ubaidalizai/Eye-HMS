0cdaef2dd451e6fe8e5020874463c62d19802f2a
saif-ullah-waziri <swaziri539@gmail.com>
2024-10-13 03:53:34 -0700

    Expense converting tables to charts

--- Frontend/src/components/SideMenu.js
+++ Frontend/src/components/SideMenu.js
@@ -89,6 +89,15 @@ function SideMenu() {
             <img alt="sale-icon" src={require("../assets/supplier-icon.png")} />
             <span className="text-sm font-medium"> Move</span>
           </Link>
+          {/* ------------------------------------- */}
+          <Link
+            to="/expenceManegement"
+            className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+          >
+            <img alt="sale-icon" src={require("../assets/supplier-icon.png")} />
+            <span className="text-sm font-medium"> Expenses</span>
+          </Link>
+        {/* --------------------------------------- */}
           <Link
             to="/pharmacy"
             className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
@@ -108,6 +117,7 @@ function SideMenu() {
                   <span className="text-sm font-medium"> Manage Store </span>
                 </div>
               </Link>
+              
             </summary>
           </details>
         </nav>
