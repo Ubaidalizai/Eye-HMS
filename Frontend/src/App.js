f062bf129fb8e68ed6a5bd4f08ce76720376b9d6
AzizRahmanZiar <azizziar1401@gmail.com>
2024-10-19 14:19:36 +0430

    Add: Branches like opration, altrasound, bedroom

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,29 +1,31 @@
-import React from "react";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Inventory from "./pages/Inventory";
-import NoPageFound from "./pages/NoPageFound";
-import AuthContext from "./AuthContext";
-import ProtectedWrapper from "./ProtectedWrapper";
-import { useEffect, useState } from "react";
-import Store from "./pages/Store";
-import Sales from "./pages/Sales";
-import Patient from "./pages/Patient";
-import PurchaseDetails from "./pages/PurchaseDetails";
-import Move from "./pages/Move";
-import Pharmacy from "./pages/Pharmacy";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import PrescriptionPage from "./pages/PrescriptionPage";
-
+import React from 'react';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import './index.css';
+import './task.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Inventory from './pages/Inventory';
+import NoPageFound from './pages/NoPageFound';
+import AuthContext from './AuthContext';
+import ProtectedWrapper from './ProtectedWrapper';
+import { useEffect, useState } from 'react';
+import Store from './pages/Store';
+import Sales from './pages/Sales';
+import Patient from './pages/Patient';
+import PurchaseDetails from './pages/PurchaseDetails';
+import Move from './pages/Move';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import PrescriptionPage from './pages/PrescriptionPage';
+import Bedroom from './components/Bedroom';
+import Ultrasound from './components/Altrasound';
+import Operation from './components/Operation';
 const App = () => {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState('');
   const [loader, setLoader] = useState(true);
-  let myLoginUser = JSON.parse(localStorage.getItem("user"));
+  let myLoginUser = JSON.parse(localStorage.getItem('user'));
   // console.log("USER: ",user)
 
   useEffect(() => {
@@ -32,7 +34,7 @@ const App = () => {
       setLoader(false);
       // console.log("inside effect", myLoginUser)
     } else {
-      setUser("");
+      setUser('');
       setLoader(false);
     }
   }, [myLoginUser]);
@@ -44,7 +46,7 @@ const App = () => {
 
   const signout = () => {
     setUser(null);
-    localStorage.removeItem("user");
+    localStorage.removeItem('user');
   };
 
   let value = { user, signin, signout };
@@ -54,9 +56,9 @@ const App = () => {
       <div
         style={{
           flex: 1,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
         <h1>LOADING...</h1>
@@ -68,10 +70,10 @@ const App = () => {
       <AuthContext.Provider value={value}>
         <BrowserRouter>
           <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
             <Route
-              path="/"
+              path='/'
               element={
                 <ProtectedWrapper>
                   <Layout />
@@ -79,19 +81,17 @@ const App = () => {
               }
             >
               <Route index element={<Dashboard />} />
-              <Route path="/inventory" element={<Inventory />} />
-              <Route path="/purchase-details" element={<PurchaseDetails />} />
-              <Route path="/sales" element={<Sales />} />
-              <Route path="/manage-store" element={<Store />} />
-              <Route path="/move" element={<Move />} />
-
-              <Route path="/patient" element={<Patient />} />
-              <Route path="/prescription/:id" element={<PrescriptionPage />} />
-              <Route path="/*" element={<Move />} />
-
-              <Route path="/pharmacy" element={<Pharmacy />} />
+              <Route path='/inventory' element={<Inventory />} />
+              <Route path='/purchase-details' element={<PurchaseDetails />} />
+              <Route path='/sales' element={<Sales />} />
+              <Route path='/manage-store' element={<Store />} />
+              <Route path='/move' element={<Move />} />
+              <Route path='/patient' element={<Patient />} />
+              <Route path='/prescription/:id' element={<PrescriptionPage />} />
+              <Route path='/branches/bedroom' element={<Bedroom />} />
+              <Route path='/branches/ultrasound' element={<Ultrasound />} />
+              <Route path='/branches/operation' element={<Operation />} />
             </Route>
-            <Route path="*" element={<NoPageFound />} />
           </Routes>
         </BrowserRouter>
       </AuthContext.Provider>
