39ee48e1ca1be392e08d53019cbc6acbc1d9bfad
fazalullahibrahimi <fazalullahrasoli10@gmail.com>
2024-10-22 05:14:01 -0700

    I added Pationt Id to PrescriptionModule

--- Backend/models/PrescriptionModule.js
+++ Backend/models/PrescriptionModule.js
@@ -1,10 +1,17 @@
 // models/Prescription.js
 
 const mongoose = require('mongoose');
-
+const Patient = require('../models/patientModel');
 const PrescriptionSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now, required: true },
   doctor: { type: String, required: true },
+
+  // Patient ID reference
+  patient_ID: {
+    type: mongoose.Schema.Types.ObjectId, // Assuming you are linking to a Patient model
+    ref: 'Patient', // If you have a Patient model, otherwise, just leave it as ObjectId
+    required: true,
+  },
 
   // Nested structure for right eye
   rightEye: {
