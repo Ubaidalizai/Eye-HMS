ea447e3e394efdc481276ac1657ca1c36753a62d
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-10-22 14:47:16 +0430

    Authenticated: income routes

--- Backend/routes/ExpensesRoute.js
+++ Backend/routes/ExpensesRoute.js
@@ -1,5 +1,7 @@
 // routes/expenseRoutes.js
 const express = require('express');
+
+const { authenticate } = require('../middlewares/authMiddleware');
 const {
   getExpenses,
   getExpensesByYear,
@@ -10,6 +12,8 @@ const {
 } = require('../controllers/ExpensesController');
 
 const router = express.Router();
+
+router.use(authenticate);
 
 // GET all expenses
 router.route('/').get(getExpenses).post(addExpense);
