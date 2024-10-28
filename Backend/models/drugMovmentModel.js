4f53dc633ddc9fd08effd957559ba2dce23dbafd
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-10-28 15:32:21 +0430

    Added: timestamps

--- Backend/models/drugMovmentModel.js
+++ Backend/models/drugMovmentModel.js
@@ -3,30 +3,33 @@ const Product = require('./product');
 const Pharmacy = require('./pharmacyModel');
 const User = require('./userModel');
 
-const drugMovementSchema = new mongoose.Schema({
-  inventory_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product',
-    required: true,
+const drugMovementSchema = new mongoose.Schema(
+  {
+    inventory_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    pharmacy_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Pharmacy',
+      required: true,
+    },
+    quantity_moved: { type: Number, required: true },
+    date_moved: { type: Date, default: Date.now },
+    moved_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: ['drug', 'sunglasses', 'frame'],
+      required: true,
+    },
   },
-  pharmacy_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Pharmacy',
-    required: true,
-  },
-  quantity_moved: { type: Number, required: true },
-  date_moved: { type: Date, default: Date.now },
-  moved_by: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  category: {
-    type: String,
-    enum: ['drug', 'sunglasses', 'frame'],
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const DrugMovement = mongoose.model('DrugMovement', drugMovementSchema);
 module.exports = DrugMovement;
