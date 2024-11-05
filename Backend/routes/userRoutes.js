f2a307d427cc3488708ed8f5ca8416675b4b72ac
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-11-05 14:54:51 +0430

    Added: image upload while user registering

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -5,13 +5,18 @@ const userController = require('../controllers/userController');
 const { authenticate } = require('../middlewares/authMiddleware');
 
 router.route('/').get(userController.getAllUsers);
-router.route('/register').post(userController.registerUser);
+router
+  .route('/register')
+  .post(
+    userController.uploadUserPhoto,
+    userController.resizeUserPhoto,
+    userController.registerUser
+  );
 router.route('/login').post(userController.loginUser);
 router.route('/logout').post(userController.logoutCurrentUser);
 router
   .route('/updatePassword')
   .patch(authenticate, userController.updatePassword);
-router.post('/updateMe', userController.uploadUserPhoto);
 router.patch(
   '/updateMe',
   authenticate,
