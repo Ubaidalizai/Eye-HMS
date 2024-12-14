87d771eb3b92a451a511fb7849e3b924d1dd448c
fazalullahibrahimi <fazalullahrasoli10@gmail.com>
2024-12-14 02:40:07 -0800

    I unique Id of this five parts

--- Backend/models/ultraSoundModule.js
+++ Backend/models/ultraSoundModule.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 
 const ultrasoundSchema = new mongoose.Schema({
-  id: { type: String, required: true },
+  id: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   time: { type: String, required: true },
   date: { type: Date, required: true },
-  image: { type: String }, // Store the image path or URL
+  image: { type: String },
   percentage: { type: Number, required: true },
 });
 
