df548454d1ecd45e6681ec586661e9781d51062f
AzizRahmanZiar <azizziar1401@gmail.com>
2024-12-31 14:45:37 +0430

    Dashboard modified

--- Frontend/src/components/BarChart.js
+++ Frontend/src/components/BarChart.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Bar } from "react-chartjs-2";
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
 
 const BarChart = ({ data, title }) => (
-  <div className='p-4 border rounded-md bg-white'>
+  <div className='p-4 mt-4 border rounded-md bg-white'>
     <h2 className='text-xl font-bold mb-4'>{title}</h2>
     <Bar
       data={data}
       options={{
         responsive: true,
         plugins: {
-          legend: { position: "top" },
+          legend: { position: 'top' },
           title: { display: true, text: title },
         },
       }}
