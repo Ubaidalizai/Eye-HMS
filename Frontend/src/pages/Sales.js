4e54f994539fbcac73c5d5fec12ff5bc71502752
AzizRahmanZiar <azizziar1401@gmail.com>
2025-01-12 14:37:41 +0430

    add sales button added

--- Frontend/src/pages/Sales.js
+++ Frontend/src/pages/Sales.js
@@ -168,21 +168,11 @@ export default function Sales() {
                 </select>
               </div>
 
-              {/* <button
-                className='inline-flex items-center px-5 py-2 border border-transparent text-sm mr-0 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none  focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                onClick={() => setShowSaleModal(true)}
-              >
-                <FaPlus className='mr-2' />
-                Add Sale
-              </button> */}
-
-              {showSaleModal && (
-                <AddSale
-                  addSaleModalSetting={() => setShowSaleModal(false)}
-                  products={products}
-                  handlePageUpdate={fetchSales}
-                />
-              )}
+              <AddSale
+                addSaleModalSetting={() => setShowSaleModal(false)}
+                products={products}
+                handlePageUpdate={fetchSales}
+              />
             </div>
           </div>
 
