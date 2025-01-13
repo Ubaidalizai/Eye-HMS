c9c30d4f460b3e01f9be2b936c00fd211dc091df
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2025-01-13 16:47:45 +0430

    Fixes bug of doctor khata outcome

--- Backend/models/doctorKhataModel.js
+++ Backend/models/doctorKhataModel.js
@@ -11,6 +11,7 @@ const doctorKhataSchema = new mongoose.Schema(
       type: String,
       required: true,
       enum: [
+        'DoctorKhata',
         'octModule',
         'opdModule',
         'labratoryModule',
